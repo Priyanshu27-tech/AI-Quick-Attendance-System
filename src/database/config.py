371c2a31dab85834fb60b803feeb34260aa5f72f
ishaan <priyanshughosh365@gmail.com>
2026-05-05 20:59:17 +0530

    initial commit

---
+++ src/database/config.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+try:
+    SUPABASE_URL = st.secrets["SUPABASE_URL"]
+    SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+    from supabase import create_client
+    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+except Exception:
+    print("⚠️ Running without Supabase")
+    supabase = None
